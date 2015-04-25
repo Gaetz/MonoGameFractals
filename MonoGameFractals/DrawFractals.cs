39b36dea6430988410e5a710ee6b790af95068c4
Gaetz <gaetz@rufflerim.com>
2015-04-25 16:27:19 +0200

    Mandelbrot fractal

---
+++ MonoGameFractals/DrawFractals.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MonoGameFractals;
+
+namespace MonoGameFractals
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class DrawFractals : Game
+    {
+        const int imageSize = 400;
+
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Fractal fractal;
+
+        public DrawFractals()
+            : base()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            fractal = new Fractal();
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            Vector2 fractalPosition = new Vector2(GraphicsDevice.Viewport.TitleSafeArea.X + GraphicsDevice.Viewport.TitleSafeArea.Width / 2 - imageSize / 2, GraphicsDevice.Viewport.TitleSafeArea.Y + GraphicsDevice.Viewport.TitleSafeArea.Height / 2 - imageSize / 2);
+            fractal.Initialize(new Texture2D(GraphicsDevice, imageSize, imageSize), fractalPosition);
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            fractal.Update();
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            // Start drawing
+            spriteBatch.Begin();
+
+            // Draw the fractal
+            fractal.Draw(spriteBatch);
+
+            // Stop drawing
+            spriteBatch.End(); 
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ MonoGameFractals/Fractal.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Numerics;
+
+namespace MonoGameFractals
+{
+    class Fractal
+    {
+        /// <summary>
+        /// Number of iteration in the escaping computing
+        /// </summary>
+        const short iteration = 100;
+
+        /// <summary>
+        /// Allows to see all fractal
+        /// </summary>
+        const float zoomDivider = 200f;
+
+        /// <summary>
+        /// Move the fractal on the right to watch it better
+        /// </summary>
+        const int xOffset = 100;
+
+        /// <summary>
+        /// Fractral texture
+        /// </summary>
+        public Texture2D FractalTexture { get; set; }
+
+        /// <summary>
+        /// Texture position
+        /// </summary>
+        public Vector2 Position { get; set; }
+
+        /// <summary>
+        /// True if fractal has been calculated and drawn
+        /// </summary>
+        bool hasDrawn;
+
+        /// <summary>
+        /// Initialize, what else ?
+        /// </summary>
+        /// <param name="texture"></param>
+        /// <param name="position"></param>
+        public void Initialize(Texture2D texture, Vector2 position)
+        {
+            FractalTexture = texture;
+            Position = position;
+
+            Color[] pixels = new Color[FractalTexture.Width * FractalTexture.Height];
+            for (int i = 0; i < pixels.Length; i++)
+            {
+                pixels[i].R = 0;
+                pixels[i].G = 0;
+                pixels[i].B = 0;
+            }
+            FractalTexture.SetData(pixels);
+            hasDrawn = false;
+        }
+
+        /// <summary>
+        /// Update and draw fractal
+        /// </summary>
+        public void Update()
+        {
+            if(!hasDrawn)
+            {
+                Color[] pixels = new Color[FractalTexture.Width * FractalTexture.Height];
+                for (int i = 0; i < pixels.Length; i++)
+                {
+                    int escapeSpeed = EscapingIteration(PixelToCoordinate(i));
+                    // Non-escaping complexes in white
+                    if (escapeSpeed >= iteration)
+                    {
+                        pixels[i].R = 255;
+                        pixels[i].G = 255;
+                        pixels[i].B = 255;
+                    }
+                    // Color in function of escaping speed
+                    else
+                        pixels[i] = GetEscapingColor(escapeSpeed);
+                }
+                FractalTexture.SetData(pixels);
+                hasDrawn = true;
+            }
+        }
+
+        /// <summary>
+        /// Get a color in function of escaping speed
+        /// </summary>
+        /// <param name="escapeSpeed"></param>
+        /// <returns></returns>
+        private Color GetEscapingColor(int escapeSpeed)
+        {
+            byte r = 0;
+            byte g = 0;
+            byte b = 0;
+            if (escapeSpeed <= 10)
+            {
+                b = (byte)Math.Round((100 - escapeSpeed) * 0.20);
+            }
+            else if (escapeSpeed > 10 && escapeSpeed <= 25)
+            {
+                b = (byte)Math.Round((100 - escapeSpeed) * 0.55);
+                g = (byte)Math.Round((100 - escapeSpeed) * 0.40);
+            }
+            else if (escapeSpeed > 25)
+            {
+                b = (byte)Math.Round((100 - escapeSpeed) * 0.75);
+                g = (byte)Math.Round((100 - escapeSpeed) * 2.55);
+                r = (byte)Math.Round((100 - escapeSpeed) * 0.55);
+            }
+            return new Color(r, g, b);
+        }
+
+        /// <summary>
+        /// True is point escapes with the fractal algorithm
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        private int EscapingIteration(Vector2 number)
+        {
+            Complex c = new Complex(number.X, number.Y);
+            Complex z = new Complex(0, 0);
+            for(int i = 0 ; i < iteration ; i++)
+            {
+                z = Iterate(z, c);
+                if (Math.Sqrt(Math.Pow(z.Real, 2) + Math.Pow(z.Imaginary, 2)) > 2)
+                    return i;
+            }
+            return iteration;
+        }
+
+        /// <summary>
+        /// Convert a texture pixel into his coordinate on the texture
+        /// </summary>
+        /// <param name="i"></param>
+        /// <returns></returns>
+        private Vector2 PixelToCoordinate(int i)
+        {
+            float x = i % (float)FractalTexture.Width - FractalTexture.Width / 2 - xOffset;
+            float y = (float)Math.Floor(i / (float)FractalTexture.Width) - FractalTexture.Width / 2;
+            return new Vector2(x / zoomDivider, y / zoomDivider);
+        }
+
+        /// <summary>
+        /// Iterative fonction, used to compute if a point is escaping or not
+        /// </summary>
+        /// <param name="zn">Iterated value (n th iteration)</param>
+        /// <param name="c">Additive seed</param>
+        /// <returns>Iterated value (n+1 th iteration)</returns>
+        private Complex Iterate(Complex zn, Complex c)
+        {
+            return Complex.Pow(zn, 2) + c;
+        }
+
+        /// <summary>
+        /// Drawing the fractal
+        /// </summary>
+        /// <param name="spriteBatch"></param>
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(FractalTexture, Position, null, Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0f); 
+        } 
+    }
+}
